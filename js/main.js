2044ffe568d8ed96046e1def3f479a757fbb5d9a
Mikhail Sapegin <mich_sp@mail.ru>
2018-02-04 17:10:48 +1000

    main

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	// Вызов слайдера owl-carousel
+	$("#slider").owlCarousel({
+		singleItem: true,
+		navigation: false,
+		theme: "slider-theme",
+		navigationText : ["",""],
+		slideSpeed: 600,
+		pagination: true
+	});
+
+});
